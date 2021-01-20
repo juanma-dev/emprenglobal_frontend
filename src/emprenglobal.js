f490f53ff902a6d960dbb9f23c4581d2117df6a7
juanma-dev <juanmanuelcastellanoshernandez@gmail.com>
2021-01-20 17:30:28 -0500

    Emprenglobal FrontEnd start

---
+++ src/emprenglobal.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+
+const Emprenglobal = (props) => {
+  return (
+    <BrowserRouter>
+      <div>TEST</div>
+    </BrowserRouter>
+  );
+};
+
+export { Emprenglobal as default };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDom from "react-dom";
+import Emprenglobal from "./emprenglobal.js";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<Emprenglobal />, document.getElementById("root"));
